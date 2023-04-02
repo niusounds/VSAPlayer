d49df5437901a52b29a014e7346c371c0538a458
Yuya Matsuo <niu@niusounds.com>
2023-04-03 00:12:57 +0900

    update Android Gradle Plugin to 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version ("7.2.2") apply (false)
-    id("com.android.library") version ("7.2.2") apply (false)
-    id("org.jetbrains.kotlin.android") version ("1.7.10") apply (false)
+    alias(libs.plugins.android.application) apply (false)
+    alias(libs.plugins.android.library) apply (false)
+    alias(libs.plugins.kotlin.android) apply (false)
 }
 
 tasks.create<Delete>("clean") {
